de9e3842fe8f21d2fc8477000a0b0832e2090ff1
irwincoelho <irwin.coelho@gmail.com>
2017-05-20 10:04:28 +0530

    Included 3 exercises of tutorial 3 in one file:

--- tut3Ex1.sh
+++ tut3Ex1.sh
@@ -6,3 +6,8 @@ echo "$1+$2="$result Length of result = ${#result}
 echo "$1+$2="$( expr $1 + $2 )
 
 echo $(( $1 + $2))
+
+if [ $result -eq 15 ]
+then 
+	echo Tomorrow is $( date --date='+1 day' )
+fi
